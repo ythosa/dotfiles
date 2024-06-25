5e7eff29cd84cfa7b11c3e5d549ecac0f48bdba6
Ruslan Babin <vasus714@yandex.ru>
2024-06-25 13:31:23 +0300

    feat: added telescope bindings

--- nvim/lua/custom/mappings.lua
+++ nvim/lua/custom/mappings.lua
@@ -45,19 +45,41 @@ M.dap_go = {
     }
 }
 
-M.disabled = {
-  n = {
-    ["gd"] = "",
-    ["gr"] = "",
-  },
-}
-
 M.lspconfig = {
   n = {
     ["gd"] = { "<cmd>Telescope lsp_definitions<CR>", "definitions" },
     ["gr"] = { "<cmd>Telescope lsp_references <CR>", "references" },
     ["gi"] = { "<cmd>Telescope lsp_implementations <CR>", "implementations" },
   },
+}
+
+M.telescope = {
+    n = {
+        ["<leader>fft"] = {
+            function ()
+                local api = require('nvim-tree.api')
+                local node = api.tree.get_node_under_cursor()
+                local path = node.absolute_path
+                require('telescope.builtin').find_files {
+                    prompt_title = "Search in " .. path,
+                    cwd = path,
+                }
+            end,
+            "Search files in selected tree node"
+        },
+        ["<leader>fwt"] = {
+            function ()
+                local api = require('nvim-tree.api')
+                local node = api.tree.get_node_under_cursor()
+                local path = node.absolute_path
+                require('telescope.builtin').live_grep {
+                    prompt_title = "Search in " .. path,
+                    cwd = path,
+                }
+            end,
+            "Search strings in selected tree node"
+        },
+    }
 }
 
 return M
--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -87,6 +87,25 @@ local plugins = {
         dependencies = {
 			"nvim-treesitter/nvim-treesitter",
 		},
+        config = function ()
+            require("custom.configs.treesitter-context").setup()
+        end
+    },
+    {
+        "princejoogie/dir-telescope.nvim",
+        dependencies = {
+            "nvim-telescope/telescope.nvim",
+        },
+        event = "BufRead",
+        config = function ()
+            require("dir-telescope").setup({
+                hidden = true,
+                no_ignore = true,
+                show_preview = true,
+            })
+            vim.keymap.set("n", "<leader>fd", "<cmd>Telescope dir live_grep<CR>", { noremap = true, silent = true })
+            vim.keymap.set("n", "<leader>pd", "<cmd>Telescope dir find_files<CR>", { noremap = true, silent = true })
+        end,
     }
 }
 return plugins
