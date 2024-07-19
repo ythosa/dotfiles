8cde7d74508a367570a97854306d0ec6b25c68ed
Ruslan Babin <vasus714@yandex.ru>
2024-07-20 01:10:03 +0300

    feat: removed completion from buffer source

--- nvim/lua/plugins/configs/cmp.lua
+++ nvim/lua/plugins/configs/cmp.lua
@@ -107,7 +107,6 @@ local options = {
   sources = {
     { name = "nvim_lsp" },
     { name = "luasnip" },
-    { name = "buffer" },
     { name = "nvim_lua" },
     { name = "path" },
     { name = "codeium" },
