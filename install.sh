c53cf1bddf6b8168d4d58663af59e0c33cd46541
rbabin <rbabin@ozon.ru>
2025-10-23 09:27:22 +0300

    feat: added install script

---
+++ install.sh
@@ -0,0 +1,6 @@
+mkdir -p ~/.config/aerospace && cp .aerospace.toml ~/.config/aerospace/
+cp .gitconfig ~/.gitconfig
+cp .tmux.conf ~/.tmux.conf
+cp .zshrc ~/.zshrc
+mkdir -p ~/.config/btop && cp btop.conf ~/.config/btop/
+mkdir -p ~/.config/k9s && cp k9s.yaml ~/.config/k9s/config.yaml
